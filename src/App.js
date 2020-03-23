58e1ad91f5e58fc7202e665cc5ee1e3404e615e1
JennyOh1004 <ohseulki1004@gmail.com>
2020-03-22 22:43:12 -0700

    Created components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import * as types from "./actions";
+import Astronomy from "./components/Astronomy";
 
-export default function App() {
-  const dispatch = useDispatch();
-  const isTest = useSelector(state => state.app.isTest);
-  console.log(isTest);
-  return (
-    <h1
-      onClick={() => dispatch({ type: types.APP_ACTION_TEST, data: !isTest })}
-    >
-      Hello Redux World! => {String(isTest)}
-    </h1>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Astronomy />
+      </div>
+    );
+  }
 }
+
+export default App;
--- src/reducers/app.js
+++ src/reducers/app.js
@@ -7,7 +7,6 @@ const initialState = {
 
 // 리듀서
 const app = (state = initialState, action) => {
-  console.log("안녕", types);
   switch (action.type) {
     case types.APP_ACTION_TEST:
       return {
@@ -18,23 +17,5 @@ const app = (state = initialState, action) => {
       return state;
   }
 };
-
-// const app = (state = initialState, action) => {
-//   switch (action.type) {
-//     case types.APP_ACTION_TEST:
-//       return {
-//         ...state,
-//         isTest: action.data
-//       };
-//     case types.APP_ACTION_DATA:
-//       return {
-//         ...state,
-//         isChecked: action.data
-//       };
-//     default:
-//       return state;
-//   }
-// };
-//각 액션 타입에 따라 다른 작업을 하기 위해 switch 구문을 사용
 
 export default app;
